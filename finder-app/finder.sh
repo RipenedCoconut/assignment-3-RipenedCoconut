4fb3cb3bd1fcf1435f33586513c02c11c30065c2
Garrett <ripenedcoconut@github.com>
2025-07-03 16:07:51 -0400

    added finder.sh search script

---
+++ finder-app/finder.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# File search script
+
+#Check for valid number of arguments
+if [ $# -lt 2 ]
+then
+	echo "ERROR: An incorrect number of arguments were specificed: $#"
+	exit 1
+fi
+
+# Argument one: file directory to search
+filesDir=$1
+
+# Argument two: the string to search for
+searchStr=$2
+
+#Check if path exists
+if [ ! -e $filesDir ]
+then
+	echo "The path $filesdir does not exist"
+	exit 1
+fi
+
+#Print the total number of directories (recursive) and name matches
+echo "The number of files are $( find $filesDir -type f | wc -l ) and the number of matching lines are $( grep -Ior $searchStr $filesDir | wc -l)"
+
+
+# Successful exit
+exit 0
